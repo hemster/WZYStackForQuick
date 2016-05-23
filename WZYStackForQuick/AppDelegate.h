74a38003988ed560afada01b8199d3270fc213dd
Zeyu Wei <wzy1@meitu.com>
2016-05-23 19:33:03 +0800

    Create project

---
+++ WZYStackForQuick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZYStackForQuick
+//
+//  Created by Zeyu Wei on 5/23/16.
+//  Copyright © 2016 Hemster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZYStackForQuick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZYStackForQuick
+//
+//  Created by Zeyu Wei on 5/23/16.
+//  Copyright © 2016 Hemster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
